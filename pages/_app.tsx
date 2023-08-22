a5c3af3ad3710e9fc92e132bce221aadc9e888ee
Liquorhands <breyerkevin2019@gmail.com>
2023-08-23 01:22:03 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,26 @@ import Navbar from "../components/Navbar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = "cronos";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+     activeChain={{
+        chainId: 25,
+        rpc: ["https://node.croswap.com/rpc", "https://evm.cronos.org/", "https://rpc.vvs.finance/", "https://rpc.crodex.app/"],
+        nativeCurrency: {
+          decimals: 18,
+          name: "Cronos",
+          symbol: "CRO",
+        },
+        shortName: "CRO",
+        slug: "Cronos",
+        testnet: false,
+        chain: "Cronos Mainnet",
+        name: "Cronos Mainnet",
+      }}
+    >
       <ChakraProvider>
         <Navbar />
         <Component {...pageProps} />
